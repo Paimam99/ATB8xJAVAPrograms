e5bce27bacccf90ceef4a5381e1d8bdf736aff52
BELIKEABHAY <abhi23171@gmail.com>
2024-09-14 20:32:32 +0530

    Add the files

---
+++ src/September/EX06_2024/LAB004.java
@@ -0,0 +1,17 @@
+package September.EX06_2024;
+
+public class LAB004 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Package - JAVA folder where the fies are saved
+        // Public class - JAVA is object-oriented project visible to everyone
+        // Main-Entry point for the JAVA program, name of function
+        //
+
+
+
+
+
+    }
+}
