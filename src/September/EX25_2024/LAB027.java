79f74d56fe20daea726a0818672d828b7a194e5c
BELIKEABHAY <abhi23171@gmail.com>
2024-10-31 15:44:25 +0530

    Add the files

---
+++ src/September/EX25_2024/LAB027.java
@@ -0,0 +1,22 @@
+package September.EX25_2024;
+
+public class LAB027 {
+    public static void main(String[] args) {
+// Loops - Foe, while, do while - a task performed multiple times
+/*        FOR LOOP
+        FOR key word | for (A:B:C)
+        A - Initialization | B - Condition for termination | C - Increment & Decrement
+ */
+        for (int i = 1; i<=10; i++){
+            System.out.println(i); // value will keeps on printing till its less than 10
+        }
+
+        for (int pai = 1; pai<6; pai++){
+            System.out.println(pai);
+        }
+
+        for (int a = 0; a > 1; a++){
+            System.out.println(a);
+        }
+    }
+}
---
+++ src/September/EX25_2024/LAB028.java
@@ -0,0 +1,28 @@
+package September.EX25_2024;
+
+public class LAB028 {
+    public static void main(String[] args) {
+//       final boolean b1 = true;
+//       for (int i= 0;b1;i++){
+//           System.out.println(b1); // infinite printing loop
+//       }
+       int a = 10; // decrement
+       for( ;a>0;a--){
+           System.out.println(a);
+       }
+
+       for (int b = 0; b<10; b++){
+           System.out.println("JAI SHREE RAM");
+       }
+       System.out.println("END");
+
+       for (int s = 1; s<=10;s++){
+           if (s==5){
+               System.out.println("Five");
+           }else{
+               System.out.println(s); // Five is printed instead of 5
+           }
+       }
+
+    }
+}
